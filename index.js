537eeca311a87aafc86a7570a164aa65279047f6
Ubuntu <ubuntu@ip-172-31-16-66.us-west-2.compute.internal>
2016-01-31 22:18:24 +0000

    safe state

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var fs = require('fs')
+var app = express()
+
+
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+
+  var buf = new Buffer(fs.readFileSync('index.html')
+
+  response.send(buf.toString())
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
